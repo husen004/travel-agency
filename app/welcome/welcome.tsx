e85e1a82cf93dd015648bf990dd4345ece66c98f
husen ikary <husenikary@mail.ru>
2025-04-26 22:36:20 +0300

    change

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,22 +1,9 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
-
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
           </div>
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
@@ -26,17 +13,7 @@ export function Welcome() {
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
+                
               ))}
             </ul>
           </nav>
